2dec8b60aefe6a86d7890ac322cf9b83f220a837
aless10 <alessio.izzo86@gmail.com>
2020-09-29 19:03:41 +0200

    [add] add project structure skeleton

---
+++ scripts/patch_swagger_template.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp /app/swagger_template/index.html /usr/local/lib/python3.8/site-packages/rest_framework_swagger/templates/rest_framework_swagger
---
+++ scripts/start_esports.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Import the environment vars
+export $(egrep -v '^#' .env | xargs)
+
+cd "${PYTHONPATH}"
+
+python /app/esports/manage.py makemigrations
+python /app/esports/manage.py migrate
+python /app/esports/manage.py createsuperuser --no-input
+python /app/esports/manage.py collectstatic --no-input
+
+DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE}" gunicorn esports.wsgi:application --bind 0.0.0.0:8000
